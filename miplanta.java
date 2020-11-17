8959139749249b518821175504ed8f484c2e02cd
Jaime Alvarez <jarabe@Jarabe.jarabe>
2020-11-17 09:30:40 +0100

    Primer commit

--- miplanta.java
+++ miplanta.java
@@ -5,30 +5,47 @@ public class miplanta{
 	boolean estaHidratada;
 
 	public void Planta(int numeroDeHojas, boolean hidratacion){
+		numeroDeHojas = 0;
+		hidratacion = false;
 		tipoDePLanta = "Geranio";
 	}
-	public gettipoDePlanta(){
+
+	public String gettipoDePlanta(){
 		return tipoDePLanta;
 	}
-	public getnumeroDeHojas(){
+
+	public int getnumeroDeHojas(){
 		return numeroDeHojas;
 	}
-	public gethidratacion(){
+
+	public boolean gethidratacion(){
 		return hidratación;
 	}
-	public settipoDePLanta(String tipoDePLanta){
 
+	public settipoDePLanta(String tipoDePLanta){
+		this.tipoDePLanta = tipoDePLanta;
 	}
-	public EcharAbono(int cantidadDeAbono){
+
+	public echarAbono(int cantidadDeAbono){
 		cantidadDeAbono;
-		numeroDeHojas = 2 + cantidadDeAbono;
+		numeroDeHojas = numeroDeHojas + cantidadDeAbono;
 	}
-	public sethidratación (Boolean hidratación){
+
+	public sethidratacion (){
+		if(hidratación == true){
+			hidratación = false;
+		}
+		else {
+			hidratacion = true;
+		}
 	}
+
 	public void estado(){
 		System.out.println("La planta es,"+ tipoDePLanta + ",con" +numeroDeHojas+ ", hidratada" +hidratacion+)
 	}
-	public void String estado(){
+
+	public String estado(){
 		estado = "La planta esta viva";
+		return estado;
 	}
 } 
